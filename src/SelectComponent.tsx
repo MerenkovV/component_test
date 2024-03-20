7a39e1cb3241098b83fd129b61163ae34c3bf22b
MerenkovV <merenkovvladislav@mail.ru>
2024-03-20 21:46:29 +0700

    Added design and selection

--- src/SelectComponent.tsx
+++ src/SelectComponent.tsx
@@ -36,6 +36,7 @@ const SelectComponent = ({className, objArray}:Props) => {
   const wrapperRef = useOutsideClick(()=>{setOpen(false)});
 
   const [objects, setObjects] = useState<ObjType[]>(objArray)
+  const [selectedObj, setSelectedObj] = useState<ObjType[]|[]>([])
   const [search, setSearch] = useState('')
   const [open, setOpen] = useState(false)
 
@@ -46,10 +47,33 @@ const SelectComponent = ({className, objArray}:Props) => {
 
   return (
     <div ref={wrapperRef} className={`select ${className}`} onClick={()=>setOpen(true)}>
+      <div className='select__target-wrapper'>
+        {
+          selectedObj.length > 0 && selectedObj.map((obj, index)=><div key={index}
+          className={`select__target ${className}`} 
+          onClick={(e)=>{
+            e.stopPropagation()
+            setSelectedObj(selectedObj.filter(select=>select.label!==obj.label))
+          }}>{obj.label}</div>)
+        }
+      </div>
+      
       <input className={`select__input ${className} ${open ? 'rounded' : ''}`} type="text" value={search} onChange={(e)=>setSearch(e.target.value)}/>
-      <div className={`select__items${open === false ? '-closed' : ''} ${className}`}>
+      <svg className={`select__icon ${className} ${open ? 'inverted' : ''}`} onClick={e=>{
+          e.stopPropagation()
+          setOpen(prev=>!prev)
+        }} viewBox="0 0 24 24"><path d="M7 10l5 5 5-5z"></path></svg>
+      <div className={`select__items${open === false ? '_closed' : ''} ${className}`}>
         {objects.map((obj, index)=>{
-          return <p className='select__item'  key={index}>{obj.label}</p>
+          return <p className={`select__item ${
+            selectedObj.find(select=>select.label===obj.label)!==undefined ? 'target' : ''
+          }`} onClick={()=>{
+            if(selectedObj.find(select=>select.label===obj.label)===undefined){
+              setSelectedObj([...selectedObj, obj])
+            }else{
+              setSelectedObj(selectedObj.filter(select=>select.label!==obj.label))
+            }
+          }} key={index}>{obj.label}</p>
         })}
       </div>
     </div>
